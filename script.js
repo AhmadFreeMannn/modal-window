c18cbce857b812e46ba05ba881e98eb1325b55eb
AhmadFreeMan <ahmadreza.mirzaei03@gmail.com>
2025-11-01 13:03:42 +0330

    add: JS for showing and closing the modal window

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overLay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  console.log(
+    btnsOpenModal[i].addEventListener("click", function () {
+      console.log("Button clicked!", i);
+      modal.classList.remove("hidden"); // * inja mesl bala select nemikonim  pas . ro nadarim
+      overLay.classList.remove("hidden");
+    })
+  );
+}
+
+btnCloseModal.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overLay.classList.add("hidden");
+});
