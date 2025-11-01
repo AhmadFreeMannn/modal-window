81ded9ca7e3c239b4face60f0ea2756445f53113
AhmadFreeMan <ahmadreza.mirzaei03@gmail.com>
2025-11-01 13:54:02 +0330

    fix: dry the code for opening the modal

--- script.js
+++ script.js
@@ -4,22 +4,21 @@ const modal = document.querySelector(".modal");
 const overLay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
-console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(
-    btnsOpenModal[i].addEventListener("click", function () {
-      console.log("Button clicked!", i);
-      modal.classList.remove("hidden"); // * inja mesl bala select nemikonim  pas . ro nadarim
-      overLay.classList.remove("hidden");
-    })
-  );
-}
+const openModal = function () {
+  console.log("modal clicked");
+  modal.classList.remove("hidden"); // * inja select nemikonim  pas . ro nadarim
+  overLay.classList.remove("hidden");
+};
 
 const clsoeModal = function () {
   modal.classList.add("hidden");
   overLay.classList.add("hidden");
 };
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", openModal);
+}
 
 btnCloseModal.addEventListener("click", clsoeModal);
 overLay.addEventListener("click", clsoeModal);
